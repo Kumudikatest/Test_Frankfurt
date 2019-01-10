7743cc7b56e829c8c6684662c093ddd8298861a5
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-10 16:34:02 +0530

    add a new cognito

--- Test_Frankfurt/lambda.js
+++ Test_Frankfurt/lambda.js
@@ -1,4 +1,18 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitoCognitoFrankfurt1,
+        Limit: "10"
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+        console.log(data);
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
